6dc6616f91c27ee8bdb777e90efedea105916e18
Nikolaj <dknikolajk@gmail.com>
2025-11-04 16:38:53 +0100

    Remove unused database helpers and related setup from RSpec configuration

--- new_app_ruby/spec/spec_helper.rb
+++ new_app_ruby/spec/spec_helper.rb
@@ -3,17 +3,11 @@ ENV['RACK_ENV'] = 'test'
 require 'capybara/rspec'
 require 'rspec'
 require_relative '../app'
-require_relative './support/database_helpers'
 
 Capybara.app = Sinatra::Application
 
 RSpec.configure do |config|
   config.include Capybara::DSL
-  config.include DatabaseHelpers
-
-  config.before(:each) do
-    clear_users
-  end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true

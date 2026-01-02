65f395653d40af1ad664b2f87a87b1f5482d55e0
Mette Munch <meha0005@stud.ek.dk>
2026-01-02 09:30:54 +0100

    exam comment to trigger workflow

--- new_app_ruby/app.rb
+++ new_app_ruby/app.rb
@@ -14,7 +14,7 @@ require 'pg'
 
 require 'prometheus/client'
 require 'prometheus/client/formats/text'
-
+# exam new comment
 # ----------------------------
 # DB (Postgres via DATABASE_URL)
 # ----------------------------

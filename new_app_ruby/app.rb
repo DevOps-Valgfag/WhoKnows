304cfedf268c16ca01a8a680e6a0fb6b37dfaa7e
Mette Munch <meha0005@stud.ek.dk>
2026-01-04 12:03:08 +0100

    Commit in order to trigger workflow

--- new_app_ruby/app.rb
+++ new_app_ruby/app.rb
@@ -14,7 +14,7 @@ require 'pg'
 
 require 'prometheus/client'
 require 'prometheus/client/formats/text'
-
+# exam
 # ----------------------------
 # DB (Postgres via DATABASE_URL)
 # ----------------------------
